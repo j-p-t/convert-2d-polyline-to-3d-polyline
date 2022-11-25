452c84bd762a2e454f2511245e67ee818bea05ad
Juha <7026019+j-p-t@users.noreply.github.com>
2022-11-25 11:23:16 -0700

    Add files via upload

---
+++ Convert_2d_polyline_to_3d_polyline.py
@@ -0,0 +1,83 @@
+import arcpy
+import os
+from datetime import datetime
+
+
+# custom exception
+class Convert2DPolylineTo3DPolylineException(Exception):
+    error_msg = ""
+
+    def __init__(self, error_msg, *args):
+        super().__init__(args)
+        self.error_msg = error_msg
+
+    def __str__(self):
+        return 'Exception: ' + self.error_msg
+
+
+# This is used to execute code if the file was run but not imported
+if __name__ == '__main__':
+    # Tool parameter accessed with GetParameter or GetParameterAsText
+    polyline = arcpy.GetParameterAsText(0)
+    elevation = arcpy.GetParameterAsText(1)
+
+    count_polyline = int(arcpy.GetCount_management(polyline).getOutput(0))
+    if count_polyline == 0:
+        raise Convert2DPolylineTo3DPolylineException("Input polyline feature class contains no records")
+    if count_polyline > 1:
+        raise Convert2DPolylineTo3DPolylineException("Input polyline feature class must contain only one record")
+
+    desc = arcpy.Describe(polyline)
+    if desc.hasZ:
+        raise Convert2DPolylineTo3DPolylineException("Input polyline feature already has an elevation field")
+
+    # get python file's current directory
+    project_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # set arcpy working directory
+    arcpy.env.workspace = project_dir
+
+    # set os working directory
+    os.chdir(project_dir)
+
+    # temporary database
+    tmp_gdb = "tmp.gdb"
+
+    # if tmp fgdb already exists, then delete
+    if os.path.exists(tmp_gdb):
+        arcpy.Delete_management(tmp_gdb)
+
+    # create new tmp fgdb
+    arcpy.management.CreateFileGDB(os.getcwd(), tmp_gdb)
+
+    # create output gdb with unique name
+    now = datetime.now()
+    output_gdb = "Polyline_3D_" + now.strftime("%d%b%Y_%H%M%S") + ".gdb"
+    arcpy.management.CreateFileGDB(project_dir, output_gdb)
+
+    tmp_pnt = tmp_gdb + "/tmp_pnt"
+
+    arcpy.management.GeneratePointsAlongLines(polyline, tmp_pnt, "DISTANCE", Distance="25 Meters")
+
+    arcpy.ddd.AddSurfaceInformation(tmp_pnt, elevation, "Z")
+
+    tmp_pnt_elev = tmp_gdb + "/tmp_pnt_elev"
+
+    arcpy.FeatureTo3DByAttribute_3d(tmp_pnt, tmp_pnt_elev, 'Z')
+
+    tmp_txt = "tmp_txt_" + now.strftime("%d%b%Y_%H%M%S") + ".txt"
+
+    arcpy.ddd.FeatureClassZToASCII(tmp_pnt_elev, project_dir, tmp_txt, "XYZ", delimiter="COMMA", decimal_separator="DECIMAL_POINT")
+
+    arcpy.ddd.ASCII3DToFeatureClass(tmp_txt, "XYZ", output_gdb + "/polyline_3d", "POLYLINE")
+
+    os.remove(tmp_txt)
+
+    # delete tmp database and its contents
+    arcpy.env.workspace = project_dir + "/" + tmp_gdb
+    fcs_to_delete = arcpy.ListFeatureClasses()
+    for fc in fcs_to_delete:
+        if arcpy.Exists(fc):
+            arcpy.Delete_management(fc)
+    arcpy.env.workspace = project_dir
+    arcpy.Delete_management(tmp_gdb)
