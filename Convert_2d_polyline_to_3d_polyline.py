b4033f933ed3b253e503c80ed53b87bef5554786
Juha <7026019+j-p-t@users.noreply.github.com>
2022-12-10 14:30:52 -0700

    Update Convert_2d_polyline_to_3d_polyline.py

--- Convert_2d_polyline_to_3d_polyline.py
+++ Convert_2d_polyline_to_3d_polyline.py
@@ -1,61 +1,94 @@
 import arcpy
-from datetime import datetime
 import os
+from datetime import datetime
+
+
+class Convert2DPolylineTo3DPolylineException(Exception):
+    error_msg = ""
+
+    def __init__(self, error_msg, *args):
+        super().__init__(args)
+        self.error_msg = error_msg
+
+    def __str__(self):
+        return 'Exception: ' + self.error_msg
 
 
 # This is used to execute code if the file was run but not imported
 if __name__ == '__main__':
-    # Tool parameter accessed with GetParameter or GetParameterAsText
-    x = arcpy.GetParameterAsText(0)
-    y = arcpy.GetParameterAsText(1)
+    polyline = arcpy.GetParameterAsText(0)
+    elevation = arcpy.GetParameterAsText(1)
     output_fc = arcpy.GetParameterAsText(2)
-    convert_to_nad_83 = arcpy.GetParameter(3)
 
-    arcpy.env.overwriteOutput = True
+    count_polyline = int(arcpy.GetCount_management(polyline).getOutput(0))
+    if count_polyline == 0:
+        raise Convert2DPolylineTo3DPolylineException("Input polyline feature class contains no records")
+    if count_polyline > 1:
+        raise Convert2DPolylineTo3DPolylineException("Input polyline feature class must contain only one record")
 
-    sr = arcpy.SpatialReference(4326)
+    desc = arcpy.Describe(polyline)
+    spatial_ref = arcpy.Describe(polyline).spatialReference
 
-    arcpy.env.outputCoordinateSystem = sr
+    arcpy.env.outputCoordinateSystem = spatial_ref
 
+    if desc.hasZ:
+        raise Convert2DPolylineTo3DPolylineException("Input polyline feature already has an elevation field")
+
+    # get python file's current directory
     project_dir = os.path.dirname(os.path.realpath(__file__))
 
+    # temporary database
+    tmp_gdb = "tmp.gdb"
+
+    # if tmp fgdb already exists, then delete
+    if os.path.exists(tmp_gdb):
+        arcpy.Delete_management(project_dir + "/" + tmp_gdb)
+
+    # create new tmp fgdb
+    arcpy.management.CreateFileGDB(project_dir, tmp_gdb)
+
+    # create output gdb with unique name
     now = datetime.now()
+    now_str = now.strftime("%d_%b_%Y_%H_%M_%S")
 
-    wgs_84_output = ""
+    tmp_pnt = project_dir + "/" + tmp_gdb + "/tmp_pnt"
 
-    # if no output specified create shp
+    arcpy.management.GeneratePointsAlongLines(polyline, tmp_pnt, "DISTANCE", Distance="10 Meters", Include_End_Points="END_POINTS")
+
+    arcpy.ddd.AddSurfaceInformation(tmp_pnt, elevation, "Z")
+
+    tmp_pnt_elev = project_dir + "/" + tmp_gdb + "/tmp_pnt_elev"
+
+    arcpy.FeatureTo3DByAttribute_3d(tmp_pnt, tmp_pnt_elev, 'Z')
+
+    tmp_txt = project_dir + "/tmp_txt_" + now.strftime("%d%b%Y_%H%M%S") + ".txt"
+
+    arcpy.ddd.FeatureClassZToASCII(tmp_pnt_elev, project_dir, tmp_txt, "XYZ", delimiter="COMMA", decimal_separator="DECIMAL_POINT")
+
     if output_fc == "":
-        project_dir = os.path.dirname(os.path.realpath(__file__))
-        wgs_84_output = project_dir + "/" + "wgs84_pnt_" + now.strftime("%d_%b_%Y_%H_%M_%S") + ".shp"
-        output_fc = wgs_84_output
+        output_fc = project_dir + "/polyline_3d_" +  now_str + ".shp"
 
-    pnt = arcpy.Point(float(x), float(y))
+    output_polyline_feature_3d = output_fc
+    arcpy.ddd.ASCII3DToFeatureClass(tmp_txt, "XYZ", output_polyline_feature_3d, "POLYLINE")
 
-    pnt_geometry = arcpy.PointGeometry(pnt)
+    # remove tmp txt file
+    os.chdir(project_dir)
+    os.remove(tmp_txt)
 
-    if not convert_to_nad_83:
-        arcpy.CopyFeatures_management(pnt_geometry, output_fc)
-    else:
-        # convert to NAD 83 datum
-        tmp = project_dir + "/" + "tmp_pnt_" + now.strftime("%d_%b_%Y_%H_%M_%S") + ".shp"
-        arcpy.CopyFeatures_management(pnt_geometry, tmp)
-        sr_out_nad_83 = arcpy.SpatialReference(4269)
-        sr = arcpy.SpatialReference(4269)
-        if output_fc == wgs_84_output:
-            output_fc = project_dir + "/" + "nad83_pnt_" + now.strftime("%d_%b_%Y_%H_%M_%S") + ".shp"
-        arcpy.management.Project(tmp, output_fc, sr_out_nad_83,
-                                 transform_method="WGS_1984_(ITRF00)_To_NAD_1983")
-        try:
-            arcpy.management.Delete(tmp)
-        except:
-            arcpy.AddWarning("Failed to delete temporary file: " + tmp)
+    # delete tmp database and its contents
+    arcpy.env.workspace = project_dir + "/" + tmp_gdb
+    fcs_to_delete = arcpy.ListFeatureClasses()
+    for fc in fcs_to_delete:
+        if arcpy.Exists(fc):
+            arcpy.Delete_management(fc)
+    arcpy.env.workspace = project_dir
+    arcpy.Delete_management(tmp_gdb)
 
     # add to map if map active
     aprx = arcpy.mp.ArcGISProject('CURRENT')
     try:
         active_map = aprx.activeMap.name
         aprxMap = aprx.listMaps(active_map)[0]
-        aprxMap.addDataFromPath(output_fc)
+        aprxMap.addDataFromPath(output_polyline_feature_3d)
     except:
         pass
-    
